005496130774ea61002aaece7fe0f26936618b46
ysihak <72233458+ysihak@users.noreply.github.com>
2021-01-24 10:49:39 -0500

    Add files via upload

---
+++ bin.js
@@ -0,0 +1,21 @@
+class Bin{
+    constructor(x,y,width,height) {
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.image=loadImage("dustbingreen.png")
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 40, 40, this.Radius, this.Radius);
+        pop();
+      }
+    }
